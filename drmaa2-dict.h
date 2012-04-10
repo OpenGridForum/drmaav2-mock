2416f8c66112959a5ace68587e4032eb78298c49
Stefan <stefan.klauck@student.hpi.uni-potsdam.de>
2012-04-11 01:01:39 +0200

    dictionary implementation

---
+++ drmaa2-dict.h
@@ -0,0 +1,18 @@
+#ifndef DRMAA2_DICT_H
+#define DRMAA2_DICT_H
+
+#include "drmaa2.h"
+
+
+typedef struct drmaa2_dict_item_s {
+	const char *key;
+	const char *value;
+	struct drmaa2_dict_item_s *next;
+} drmaa2_dict_item_s;
+typedef drmaa2_dict_item_s *drmaa2_dict_item;
+
+typedef struct drmaa2_dict_s {
+	drmaa2_dict_item head;
+} drmaa2_dict_s;
+
+#endif DRMAA2_DICT_H
